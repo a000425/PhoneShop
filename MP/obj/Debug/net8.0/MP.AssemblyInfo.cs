29f8c316ed8af8745a6a2402167df2e5cac932ad
a000425 <ttes20030425@gmail.com>
2024-03-28 18:10:24 +0800

    remove

--- MP/obj/Debug/net8.0/MP.AssemblyInfo.cs
+++ MP/obj/Debug/net8.0/MP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28256317df125e5c07427eaf13316745d0348440")]
 [assembly: System.Reflection.AssemblyProductAttribute("MP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
