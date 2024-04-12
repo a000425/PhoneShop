d0eb35baf18d5804db89c4cd86745f31a6752da2
haogotobenso <hao104078@gmail.com>
2024-04-12 18:07:28 +0800

    QA Front and Back (BackService Unrepository)

---
+++ MP/Dtos/FrontQADto.cs
@@ -0,0 +1,15 @@
+﻿namespace MP.Dtos
+{
+    public class FrontQADto
+    {
+        public string Account { get; set; } = null!;
+
+        public string Content { get; set; } = null!;
+
+        public DateTime CreateTime { get; set; }
+
+        public string? Reply { get; set; }
+
+        public DateTime? ReplyTime { get; set; }
+    }
+}
